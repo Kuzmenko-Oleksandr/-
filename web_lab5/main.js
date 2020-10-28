d3a85cf870f9e03693c9ecc58534a700cced5972
Oleksandr <shparman17@gmail.com>
2020-10-28 11:05:19 +0200

    laba5

---
+++ web_lab5/main.js
@@ -0,0 +1,10 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
+$('.vid').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
